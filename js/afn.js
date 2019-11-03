23094eec71e06a06ae1416b5a7a4bbb5b2f714ac
Geane Bocaiuva <40280143+bocaiuva@users.noreply.github.com>
2019-11-03 00:19:10 -0300

    first comm

---
+++ js/afn.js
@@ -0,0 +1,129 @@
+var alfabeto;
+var inicial;
+var final;
+var estados = [];
+var $ = go.GraphObject.make;
+
+var model = $(go.GraphLinksModel);
+
+function dividir() {
+  skeleton();
+  var entrada = document.getElementById("afn").value;
+  var arrayLi = entrada.split('\n');
+  for (var i = 0; i < arrayLi.length; i++) {
+    organizar(arrayLi[i].split(':'))
+  }
+}
+function organizar(lines) {
+  for (let i = 0; i < lines.length; i++) {
+
+    if (lines[i] == 'AB') {
+      i++;
+       alfabeto = lines[i];
+      console.log("alfabeto é " + alfabeto);
+    } else if (lines[i] == 'i') {
+      i++;
+      inicial = lines[i]; //pegar info do esrado inicial
+      console.log("inicial é " + inicial);
+    } else if (lines[i] == 'f') {
+      i++;
+      final = lines[i]; //pegar info do esrado final
+      console.log("final é " + final);
+    } else {
+      var est = lines[i].split(' ');
+
+      if (!alfabeto.includes(est[01])) {
+        document.getElementById("myDiagramDiv").style.display = "none";
+        document.getElementById("divBtn").innerHTML = "";
+        document.getElementById("divAfn").style.display = "none";
+        document.getElementById("erro").innerHTML = "Oop. Formato errado, seu alfabeto é " + alfabeto;
+        document.getElementById("erro").style.display = "block";
+        document.getElementById("restart").style.display = "block";
+      }
+
+      if (!estados.includes(est[0])) {
+        var node = { key: est[0], name: est[0] };
+        model.addNodeData(node);
+        estados.push(est[0]);
+
+        if (est[0] != est[2] && !estados.includes(est[2])) {
+          node = { key: est[2], name: est[2] };
+          model.addNodeData(node);
+          estados.push(est[2]);
+        }
+      }
+
+      if (est[2]) {
+        if (est[2].indexOf(',') != -1) {
+          var firstComingOfChrist = est[2].substring(0, est[2].indexOf(','));
+          var secondComingOfChrist = est[2].substring(est[2].indexOf(',') + 1, est[2].length);
+          console.log("christ" + secondComingOfChrist);
+          var link = { from: est[0], to: firstComingOfChrist, text: est[1] };
+          model.addLinkData(link);
+
+          link = { from: est[0], to: secondComingOfChrist, text: est[1] };
+          model.addLinkData(link);
+        }
+      }
+      link = { from: est[0], to: est[2], text: est[1] };
+      model.addLinkData(link);
+    }
+  }
+}
+
+function skeleton() {
+  var myDiagram = $(go.Diagram, "myDiagramDiv");
+  myDiagram.nodeTemplate =
+    $(go.Node, "Auto",
+      $(go.Shape, "Circle",
+        {
+          fill: $(go.Brush, "Linear", { 0: "rgb(0, 150, 173)", 1: "rgb(0, 87, 173)" }),
+          stroke: "rgb(0, 150, 173)", strokeWidth: 0,
+          fromLinkable: true, fromLinkableSelfNode: true, fromLinkableDuplicates: true,
+          toLinkable: true, toLinkableSelfNode: true, toLinkableDuplicates: true
+        }),
+      $(go.TextBlock, "Default Text",
+        { margin: 12, stroke: "white", font: "bold 16px sans-serif" },
+        new go.Binding("text", "name"))
+    );
+
+  myDiagram.linkTemplate =
+    $(go.Link,
+      {
+        curve: go.Link.Bezier, adjusting: go.Link.Stretch,
+        toShortLength: 3
+      },
+      new go.Binding("points"),
+      new go.Binding("curviness"),
+      $(go.Shape,
+        { strokeWidth: 1.5 }),
+      $(go.Shape,
+        { toArrow: "standard", stroke: null }),
+      $(go.Panel, "Auto",
+        $(go.Shape,
+          {
+            fill: $(go.Brush, "Radial",
+              { 0: "rgb(255, 255, 255)", 0.3: "rgb(255, 255, 255)", 1: "rgba(255, 255, 255, 0)" }),
+            stroke: null
+          }),
+        $(go.TextBlock, "transition",
+          {
+            textAlign: "center",
+            font: "9pt helvetica, arial, sans-serif",
+            margin: 4
+          },
+          new go.Binding("text"))
+      )
+    );
+  /* model.nodeDataArray = [];
+   model.linkDataArray = [];*/
+  myDiagram.model = model;
+  document.getElementById("myDiagramDiv").style.display = "block";
+  document.getElementById("restart").style.display = "block";
+  document.getElementById("divBtn").innerHTML = "";
+  document.getElementById("divAfn").style.display = "none";
+}
+
+function reload() {
+  location.reload();
+}
